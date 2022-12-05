21f968cdabd54083d60ef7fb2c5696c6a6f108d0
randomn4m3 <114420248+randomn4m3@users.noreply.github.com>
2022-12-05 21:01:38 +0100

    Day 5 2022

---
+++ 2022/5/a.ts
@@ -0,0 +1,52 @@
+const inputs = await Deno.readTextFile("./input.txt");
+
+const lines = inputs.split("\n");
+
+const regexCrates = /\s{4}|[A-Z]/g;
+const regexMoves = /\d+/g;
+
+let initialization = true;
+const crates: string[][] = [];
+const moves = [];
+
+for (const line of lines) {
+  if (initialization) {
+    if (line.startsWith(" 1   2")) {
+      initialization = false;
+      continue;
+    }
+    const row = line.match(regexCrates);
+    for (const i in row) {
+      const index = parseInt(i, 10);
+      // If doesn't exist init
+      if (!crates[index]) {
+        crates[index] = [];
+      }
+      // empty has 4 spaces, length 4
+      if (row[index].length === 1) {
+        crates[index].push(row[index]);
+      }
+    }
+  } else {
+    const row = line.match(regexMoves);
+    if (row) {
+      moves.push(row.map((n) => parseInt(n, 10)));
+    }
+  }
+}
+console.log("init");
+console.log(crates);
+console.log("moves");
+console.log(moves);
+
+for (const e of moves) {
+  for (let i = 0; i < e[0]; i++) {
+    const pop = crates[e[1] - 1].shift();
+    if (crates[e[2] - 1] && pop) {
+      crates[e[2] - 1].unshift(pop);
+    }
+  }
+}
+
+const result = crates.map((e) => e[0]).join("");
+console.log(result);
